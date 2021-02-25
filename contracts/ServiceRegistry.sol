1d6b9eb210b274c5db93206f89688e3ccf2f5456
vlatlant <vl@atlant.io>
2021-02-25 11:37:21 +0200

    fix license

--- contracts/ServiceRegistry.sol
+++ contracts/ServiceRegistry.sol
@@ -1,4 +1,4 @@
-// Copyright 2017, 2018 Tensigma Ltd. All rights reserved.
+// Copyright 2017-2021 Digital Asset Exchange Limited. All rights reserved.
 // Use of this source code is governed by Microsoft Reference Source
 // License (MS-RSL) that can be found in the LICENSE file.
 
